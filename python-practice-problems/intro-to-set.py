0aef0f223cd8d5792219df18777c5870688d97a5
elzhan <elzhan.zeinulla@nu.edu.kz>
2018-03-19 02:34:58 +0600

    intro-to-sets

---
+++ python-practice-problems/intro-to-set.py
@@ -0,0 +1,11 @@
+def average(array):
+    return sum(set(array))/len(set(array))
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
+
