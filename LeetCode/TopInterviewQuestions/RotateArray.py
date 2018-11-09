58ec13138a4c4c35c85ea248395bd36c6dd33373
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-11-10 01:16:35 +0600

    Finished

--- LeetCode/TopInterviewQuestions/RotateArray.py
+++ LeetCode/TopInterviewQuestions/RotateArray.py
@@ -24,3 +24,21 @@ class Solution(object):
         for i in range(k):
             self.rotate_by_one(nums)
             
+            
+            
+#======================================================================
+#Solution 2:
+
+# O(N*k)
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+
+        for i in range(k):
+            nums.insert(0, nums.pop())
+        
+            
