a8f73b34518e4b806fe94ff07f492e36b6241e07
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-11-11 17:39:50 +0600

    Finished

--- LeetCode/TopInterviewQuestions/TwoSum.py
+++ LeetCode/TopInterviewQuestions/TwoSum.py
@@ -1,8 +1,32 @@
 '''Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.'''
+from collections import defaultdict
 
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashtable = defaultdict(int)
+        
+        for index in range(len(nums)):
+            hashtable[nums[index]] = index
+        
+        for index in range(len(nums)):
+            var = hashtable[target-nums[index]]
+            if var != 0 and var != index:
+                return [index, hashtable[target-nums[index]]]
+        
+        # target = 6
+        # 1 0
+        # 3 1
+        # 4 2
+        # 2 3
 
+#=======================================================================================
 class Solution:
     def twoSum(self, nums, target):
         temp = nums.copy()
