2e3832538b9f41ac284c27ba6c7a187170b7ba85
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-11-13 23:25:55 +0600

    Finished

--- LeetCode/TopInterviewQuestions/PlusOne.py
+++ LeetCode/TopInterviewQuestions/PlusOne.py
@@ -19,3 +19,21 @@ class Solution:
                 digits[i-1] = digits[i-1] + digits[i]//10                
                 digits[i] = digits[i]%10
         return digits
+# follow-up
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digits[-1]+=1
+        temp = 0
+        for i in range(len(digits)-1, -1, -1):
+            digits[i]+=temp
+            temp = digits[i]//10
+            digits[i] = digits[i]%10
+            if temp == 0:
+                break
+        if temp != 0:
+            digits.insert(0, 1)
+        return digits
