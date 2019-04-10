6f7b1f96949ad60fcf0fc65cc9da372e951ab9df
Elzhan <elzhan.zeinulla@nu.edu.kz>
2019-04-11 01:02:05 +0600

    Finished

---
+++ LeetCode/LinkedListCycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        first, second = head, head
+        while second and second.next:
+            second = second.next.next
+            first = first.next
+            if first is second:
+                return True
+        return False
