e2b5d3ad7dafa900f4bcb1e9bc4b5f9f847ead14
Elzhan <elzhan.zeinulla@nu.edu.kz>
2019-10-02 14:35:27 +0600

    Finished

---
+++ Hackerrank-Interview-Preparation-Kit/insert_a_node.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+    temp_head = head
+    while position > 1:
+        temp_head = temp_head.next        
+        position-=1
+
+    new_node = SinglyLinkedListNode(data)
+    new_node.next = temp_head.next
+    temp_head.next = new_node
+
+    return head    
+
+if __name__ == '__main__':
